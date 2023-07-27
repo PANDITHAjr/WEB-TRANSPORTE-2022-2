cf6ab8441d934c4ac837ea696276604bb82497ff
PANDITHAjr <rcarballo.carlos@gmail.com>
2023-07-26 23:15:37 -0400

    FINAL DISEÑO

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -4,56 +4,98 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-
                 <div class="row">
-
-                    <br>
-                    <br>
                     <div class="col s12 m4">
                         <div class="card">
                             <div class="card-image">
                                 <img class="card-img" src="{{ asset('images/logoizquierdo.png') }}">
-                                <span class="card-title">CARLOS</span>
+                                {{-- <span class="card-title">CARLOS</span> --}}
                             </div>
                             <div class="card-action">
-                                <a href="{{ route('sindicato.index') }}", class="btn btn-floating red pulse"><i class="material-icons">group</i></a>
+                                <a href="{{ route('sindicato.index') }}", class="btn btn-floating red pulse"><i
+                                        class="material-icons">group</i></a>
                                 <a style="color:brown; font-size: 14px; font-family: Georgia;">SINDICATOS</a>
                             </div>
                         </div>
                     </div>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
+
                     <div class="col s12 m4">
                         <div class="card">
                             <div class="card-image">
-
                                 <img class="card-img" src="{{ asset('images/logomedio.png') }}">
-                                <span class="card-title">WEIMAR</span>
+                                {{-- <span class="card-title">CARLOS</span> --}}
                             </div>
                             <div class="card-action">
-                                <a href="{{ route('personal.index') }}", class="btn btn-floating yellow pulse"><i class="material-icons">airport_shuttle</i></a>
+                                <a href="{{ route('personal.index') }}", class="btn btn-floating yellow pulse"><i
+                                        class="material-icons">airline_seat_recline_normal</i></a>
                                 <a style="color:brown; font-size: 14px; font-family: Georgia;">PERSONAL</a>
                             </div>
                         </div>
                     </div>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
                     <div class="col s12 m4">
                         <div class="card">
                             <div class="card-image">
                                 <img class="card-img" src="{{ asset('images/logoderecho.png') }}">
-                                <span class="card-title">AYELEN</span>
+                                {{-- <span class="card-title">CARLOS</span> --}}
                             </div>
                             <div class="card-action">
-                                <a href="{{ route('vehiculo.index') }}", class="btn btn-floating green pulse"><i class="material-icons">airline_seat_recline_normal</i></a>
-                                <a style="color:brown; font-size: 14px; font-family: Georgia;"> VEHICULO</a>
+                                <a href="{{ route('sindicato.index') }}", class="btn btn-floating green pulse"><i
+                                        class="material-icons">airport_shuttle</i></a>
+                                <a style="color:brown; font-size: 14px; font-family: Georgia;">VEHICULOS</a>
                             </div>
                         </div>
                     </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img class="card-img" src="{{ asset('images/izquierdaABAJO.png') }}">
+                                {{-- <span class="card-title">CARLOS</span> --}}
+                            </div>
+                            <div class="card-action">
+                                <a href="{{ route('sindicato.index') }}", class="btn btn-floating red pulse"><i
+                                        class="material-icons">group</i></a>
+                                <a style="color:brown; font-size: 14px; font-family: Georgia;">RUTAS</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img class="card-img" src="{{ asset('images/Bienvenidos.png') }}">
+                                {{-- <span class="card-title">CARLOS</span> --}}
+                            </div>
+                            {{-- <div class="card-action">
+                                <a href="{{ route('personal.index') }}", class="btn btn-floating yellow pulse"><i
+                                        class="material-icons">airline_seat_recline_normal</i></a>
+                                <a style="color:brown; font-size: 14px; font-family: Georgia;">PERSONAL</a>
+                            </div> --}}
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img class="card-img" src="{{ asset('images/derechaABAJO.png') }}">
+                                {{-- <span class="card-title">CARLOS</span> --}}
+                            </div>
+                            <div class="card-action">
+                                <a href="{{ route('sindicato.index') }}", class="btn btn-floating green pulse"><i
+                                        class="material-icons">airport_shuttle</i></a>
+                                <a style="color:brown; font-size: 14px; font-family: Georgia;">PROMOCIONES</a>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
