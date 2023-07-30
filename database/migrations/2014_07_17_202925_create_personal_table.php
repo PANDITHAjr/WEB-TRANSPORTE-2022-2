769707372e192188cb1ef2bdf2977be7b423ed06
Aye-ES <ayelenestevezs@gmail.com>
2023-07-29 14:51:04 -1000

    agreganfo avance docweb xd mas drawio

--- database/migrations/2014_07_17_202925_create_personal_table.php
+++ database/migrations/2014_07_17_202925_create_personal_table.php
@@ -6,11 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreatePersonalTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+   
     public function up()
     {
         Schema::create('personal', function (Blueprint $table) {
@@ -32,11 +28,7 @@ class CreatePersonalTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+   
     public function down()
     {
         Schema::dropIfExists('personal');
